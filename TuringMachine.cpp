97ee1e0bfa0f06513a388362040ee57b04c17af7
Fuentes García Alan Kevin <fuentes.alan.98@gmail.com>
2017-11-16 09:56:33 -0600

    Create TuringMachine.cpp

---
+++ TuringMachine.cpp
@@ -0,0 +1,9 @@
+struct TuringMachine {
+	vector<int> estados;
+	vector<char> alfabetoEntrada;
+	vector<char> alfabetoCinta;
+	int inicial;
+	const char blank = '~';
+	vector<int> finales;
+	vector<map<char, Terna>> delta;
+};
