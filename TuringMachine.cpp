bc7ffd8621a04522156b5e142999b10ee81a805f
Fuentes García Alan Kevin <fuentes.alan.98@gmail.com>
2017-11-23 09:39:33 -0600

    Update TuringMachine.cpp

--- TuringMachine.cpp
+++ TuringMachine.cpp
@@ -19,9 +19,36 @@ struct TuringMachine {
 		sort(alfabetoCinta.begin(),alfabetoCinta.end());
 		sort(finales.begin(),finales.end());
 	}
+	
 	void agregar(int estado, char c, Terna t){
 		delta[estado][c] = t; 
-        }
+    }
+	
+	bool leerEntrada (string entrada){
+		list<char> cinta (cadena.begin(), cadena.end());
+		int estadoActual = inicial;
+		auto simboloActual = cinta.begin();	/* auto -> list::iterator<char> */
+		while (delta[estadoActual].count(*simboloActual) > 0){
+			Terna t = delta[estadoActual][*simboloActual];
+			estadoActual = t.estado;
+			*simboloActual = t.simbolo;
+			switch (t.direccion){
+				case -1:
+					if (simboloActual == cinta.begin()){
+						cinta.push_front(blank);
+					}
+					simboloActual--;
+					break;
+				case 1:
+					simboloActual ++;
+					if(simboloActual == cinta.end()){
+						cinta.push_back(blank);
+					}
+					break;
+			}
+		}
+		return binary_search(finales.begin(),finales.end(),estadoActual);
+	}
 	
 };
 
