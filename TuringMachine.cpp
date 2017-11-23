e28080b811d09d3547e46f8a58ec712b72840d14
irving97ll <33726441+irving97ll@users.noreply.github.com>
2017-11-23 09:04:05 -0600

    Update TuringMachine.cpp

--- TuringMachine.cpp
+++ TuringMachine.cpp
@@ -5,7 +5,18 @@ struct TuringMachine {
 	vector<char> alfabetoEntrada;
 	vector<char> alfabetoCinta;
 	int inicial;
-	const char blank = '~';
+	const char blank = 'B';
 	vector<int> finales;
 	vector<map<char, Terna>> delta;
+	TuringMachine(int numeroEstados, vector<char> alfabetoEntrada, vector<char> alfabetoCinta,int inicial, vector<int> finales){
+		this->numeroEstados= numeroEstados;
+		this->alfabetoEntrada= alfabetoEntrada;
+		this->alfabetoCinta= alfabetoCinta;
+		this->inicial= inicial;
+		this->finales= finales;
+		delta.resize(numeroEstados);
+		sort(alfabetoEntrada.begin(),alfabetoEntrada.end());
+		sort(alfabetoCinta.begin(),alfabetoCinta.end());
+		sort(finales.begin(),finales.end());
+	}
 };
