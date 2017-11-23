1d4f9dd84d29131b029dc36bf1619fd2868591ca
Alan Enrique Ontiveros Salazar <alan-enrique@live.com.mx>
2017-11-23 09:49:13 -0600

    Update TuringMachine.cpp

--- TuringMachine.cpp
+++ TuringMachine.cpp
@@ -14,27 +14,28 @@ struct TuringMachine {
 	const char blank = 'B';
 	vector<int> finales;
 	vector<map<char, Terna>> delta;
-	TuringMachine(int numeroEstados, vector<char> alfabetoEntrada, vector<char> alfabetoCinta,int inicial, vector<int> finales){
+	
+	TuringMachine(int numeroEstados, vector<char> alfabetoEntrada, vector<char> alfabetoCinta, int inicial, vector<int> finales){
 		this->numeroEstados= numeroEstados;
 		this->alfabetoEntrada= alfabetoEntrada;
 		this->alfabetoCinta= alfabetoCinta;
 		this->inicial= inicial;
 		this->finales= finales;
 		delta.resize(numeroEstados);
-		sort(alfabetoEntrada.begin(),alfabetoEntrada.end());
-		sort(alfabetoCinta.begin(),alfabetoCinta.end());
-		sort(finales.begin(),finales.end());
+		sort(alfabetoEntrada.begin(), alfabetoEntrada.end());
+		sort(alfabetoCinta.begin(), alfabetoCinta.end());
+		sort(finales.begin(), finales.end());
 	}
 	
 	void agregar(int estado, char c, Terna t){
 		delta[estado][c] = t; 
-    }
+    	}
 	
-	bool leerEntrada (string entrada){
+	bool leerEntrada (string & entrada){
 		list<char> cinta (entrada.begin(), entrada.end());
 		int estadoActual = inicial;
 		auto simboloActual = cinta.begin();	/* auto -> list::iterator<char> */
-		while (delta[estadoActual].count(*simboloActual) > 0){
+		while (delta[estadoActual].count(*simboloActual) > 0 && !binary_search(finales.begin(), finales.end(), estadoActual)){
 			Terna t = delta[estadoActual][*simboloActual];
 			estadoActual = t.estado;
 			*simboloActual = t.simbolo;
@@ -53,7 +54,14 @@ struct TuringMachine {
 					break;
 			}
 		}
-		return binary_search(finales.begin(),finales.end(),estadoActual);
+		while(cinta.back() == blank){
+			cinta.pop_back();
+		}
+		while(cinta.front() == blank){
+			cinta.pop_front();
+		}
+		entrada = string(cinta.begin(), cinta.end());
+		return binary_search(finales.begin(), finales.end(), estadoActual);
 	}
 	
 };
