c4fce3050018b06a8c6b3a6e6ac88c1508d83a56
Alan Enrique Ontiveros Salazar <alan-enrique@live.com.mx>
2017-11-30 10:29:39 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ Proyecto final de teoría computacional
 		-Ontiveros Salazar Alan Enrique
 		-Ruiz Lopez Luis Carlos
 	Descripción: implementación de la Máquina de Turing
-	universal determinista de una sola cinta
+	determinista de una sola cinta
 	Aplicación: convertidor de decimal a binario
 */
 
@@ -17,5 +17,60 @@ Proyecto final de teoría computacional
 using namespace std;
 
 int main(){
+	int estados, estadoInicial;
+	int numEstadosFinales, numTransiciones;
+	char blank;
+	string alfabetoCinta;
+
+	cout << "Introduce el numero de estados: ";
+	cin >> estados;
+
+	cout << "Introduce el alfabeto de cinta: ";
+	cin >> alfabetoCinta;
+
+	cout << "Introduce el simbolo blanco: ";
+	cin >> blank;
+
+	cout << "Introduce el estado inicial: ";
+	cin >> estadoInicial;
+
+	cout << "Introduce el numero de estados finales: ";
+	cin >> numEstadosFinales;
+
+	cout << "Introduce los " << numEstadosFinales << " estados finales:\n";
+
+	vector<int> estadosFinales(numEstadosFinales);
+	for(int i = 0; i < numEstadosFinales; i++){
+		cin >> estadosFinales[i];
+	}
+
+	cout << "Introduce el numero de transiciones: ";
+	cin >> numTransiciones;
+
+	cout << "Introduce las " << numTransiciones << " en la forma delta(q, a) = (p, b, d):\n";
+
+	TuringMachine * MT = new TuringMachine(estados, vector<char>(alfabetoCinta.begin(), alfabetoCinta.end()), blank, estadoInicial, estadosFinales);
+
+	int q, p, d;
+	char a, b;
+
+	for(int i = 0; i < numTransiciones; i++){
+		cin >> q >> a >> p >> b >> d;
+		MT->agregar(q, a, {p, b, d});
+	}
+
+	string cadena;
+
+	do{
+		cout << "Introduce una cadena, " << blank << " para salir: ";
+		cin >> cadena;
+		if(cadena[0] == blank) break;
+		if(MT->leerEntrada(cadena)){
+			cout << "Cadena valida. Contenido de la cinta: " << cadena << "\n\n";
+		}else{
+			cout << "Cadena no valida.\n\n";
+		}
+	}while(cadena[0] != blank);
+
 	return 0;
 }
