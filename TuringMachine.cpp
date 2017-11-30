ab108519f5c155bc40cf9d7c4d6a0755ae28d447
Alan Enrique Ontiveros Salazar <alan-enrique@live.com.mx>
2017-11-30 10:29:17 -0600

    Update TuringMachine.cpp

--- TuringMachine.cpp
+++ TuringMachine.cpp
@@ -3,56 +3,83 @@
 #include <list>
 #include <map>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 struct TuringMachine {
 	int numeroEstados;
-	vector<char> alfabetoEntrada;
 	vector<char> alfabetoCinta;
 	int inicial;
-	const char blank = 'B';
+	char blank;
 	vector<int> finales;
 	vector<map<char, Terna>> delta;
 	
-	TuringMachine(int numeroEstados, vector<char> alfabetoEntrada, vector<char> alfabetoCinta, int inicial, vector<int> finales){
-		this->numeroEstados= numeroEstados;
-		this->alfabetoEntrada= alfabetoEntrada;
-		this->alfabetoCinta= alfabetoCinta;
-		this->inicial= inicial;
-		this->finales= finales;
+	TuringMachine(int numeroEstados, vector<char> alfabetoCinta, char blank, int inicial, vector<int> finales){
+		this->numeroEstados = numeroEstados;
+		this->alfabetoCinta = alfabetoCinta;
+		this->blank = blank;
+		this->inicial = inicial;
+		this->finales = finales;
 		delta.resize(numeroEstados);
-		sort(alfabetoEntrada.begin(), alfabetoEntrada.end());
-		sort(alfabetoCinta.begin(), alfabetoCinta.end());
 		sort(finales.begin(), finales.end());
 	}
 	
 	void agregar(int estado, char c, Terna t){
 		delta[estado][c] = t; 
+    }
+
+    void imprimirCinta(list<char> cinta, int estado, int posicion){
+    	cout << "Estado actual: " << estado << "\n";
+    	cout << "Contenido de la cinta:\n";
+    	for(char c : cinta){
+    		cout << c;
     	}
+    	cout << "\n";
+    	for(int i = 0; i < cinta.size(); i++){
+    		if(i == posicion){
+    			cout << "|";
+    		}else{
+    			cout << "-";
+    		}
+    	}
+    	cout << "\n";
+    }
 	
 	bool leerEntrada (string & entrada){
 		list<char> cinta (entrada.begin(), entrada.end());
 		int estadoActual = inicial;
 		auto simboloActual = cinta.begin();	/* auto -> list::iterator<char> */
+		int posicion = 0;
+		imprimirCinta(cinta, estadoActual, posicion);
 		while (delta[estadoActual].count(*simboloActual) > 0 && !binary_search(finales.begin(), finales.end(), estadoActual)){
 			Terna t = delta[estadoActual][*simboloActual];
-			estadoActual = t.estado;
 			*simboloActual = t.simbolo;
 			switch (t.direccion){
-				case -1:
-					if (simboloActual == cinta.begin()){
-						cinta.push_front(blank);
+				case -1:{
+						if (simboloActual == cinta.begin()){
+							cinta.push_front(blank);
+							simboloActual = cinta.begin();
+							posicion = 0;
+						}else{
+							simboloActual--;
+							posicion--;
+						}
+						break;
 					}
-					simboloActual--;
-					break;
-				case 1:
-					simboloActual ++;
-					if(simboloActual == cinta.end()){
-						cinta.push_back(blank);
+				case 1:{
+						simboloActual++;
+						posicion++;
+						if(simboloActual == cinta.end()){
+							cinta.push_back(blank);
+							simboloActual = cinta.end();
+							simboloActual--;
+						}
+						break;
 					}
-					break;
 			}
+			estadoActual = t.estado;
+			imprimirCinta(cinta, estadoActual, posicion);
 		}
 		while(cinta.back() == blank){
 			cinta.pop_back();
