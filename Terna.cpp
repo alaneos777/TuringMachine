fa71b487f6ff0e71f39732ec767f8ac2e541f5da
Fuentes García Alan Kevin <fuentes.alan.98@gmail.com>
2017-12-09 00:34:29 -0600

    Update Terna.cpp

--- Terna.cpp
+++ Terna.cpp
@@ -1,5 +1,5 @@
-struct Terna{
-	int estado;
-	char simbolo;
-	int direccion;
+struct Terna{		//Definición de terna para transiciones de función delta
+	int estado;		//Estado a transitar
+	char simbolo;		//Símbolo a reemplazar en la cinta de la MT
+	int direccion;		//Dirección a mover la cinta (1: derecha, 0: estática, -1: izquierda)
 };
